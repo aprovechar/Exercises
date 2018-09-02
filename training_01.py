317ecc85654560c85728bb09471832e683838315
Professor Oak <amsyar0402@gmail.com>
2018-09-02 20:02:30 +0800

    Updated!

--- training_01.py
+++ training_01.py
@@ -2,7 +2,4 @@ print("I really need to Zzzzzz")
 print("Hey Xin, now we're one! hahah ^_^")
 print("Added some dev setting!")
 print("Testing git remote sync")
-<<<<<<< HEAD
-=======
-print("test")
->>>>>>> 106e85979378966e1dfd352b4f404373dcfb0e96
+
