eb696e8e83d1f15f7a3706df2ed14fb84a5823bb
Amsyar <amsyar0402@gmail.com>
2018-09-18 00:28:39 +0800

    Updated carry_array.py!

--- Am's Project/carry_array.py
+++ Am's Project/carry_array.py
@@ -1,3 +1,17 @@
 def add_one(given_array):
-    carry=1
+    carry = 1
+    result = int[given_array.length]
+
+    for i from (given_array.length - 1) down to 0:
+        total = given_array[i] + carry
+        if total == 10:
+            carry = 1
+        else:
+            carry = 0
+            result[i] = total % 10
+        if carry == 1:
+            result = int[given_array.length + 1]
+            result[0] = 1
+        return result
+
 
--- MasterClass_Tutorial/variables.py
+++ MasterClass_Tutorial/variables.py
@@ -1,2 +1,3 @@
 age = 24
 print("My age is " + str(age) + " years old!")
+print("Pull and push using pycharm")
