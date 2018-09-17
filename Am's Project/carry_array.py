264a3081307cf86d7a43f759eaafd098585b3f27
Amsyar <amsyar0402@gmail.com>
2018-09-18 00:34:35 +0800

    Updated carry_array.py!

--- Am's Project/carry_array.py
+++ Am's Project/carry_array.py
@@ -1,17 +1,22 @@
-def add_one(given_array):
-    carry = 1
-    result = int[given_array.length]
+# def add_one(given_array):
+#     carry = 1
+#     result = int[given_array.length]
+#
+#     for i from (given_array.length - 1) down to 0:
+#         total = given_array[i] + carry
+#         if total == 10:
+#             carry = 1
+#         else:
+#             carry = 0
+#             result[i] = total % 10
+#         if carry == 1:
+#             result = int[given_array.length + 1]
+#             result[0] = 1
+#         return result
 
-    for i from (given_array.length - 1) down to 0:
-        total = given_array[i] + carry
-        if total == 10:
-            carry = 1
-        else:
-            carry = 0
-            result[i] = total % 10
-        if carry == 1:
-            result = int[given_array.length + 1]
-            result[0] = 1
-        return result
-
-
+def addOne(givenArray):
+    arrayStr = ""
+    for i in givenArray:
+        arrayStr += str(i)
+        arrayInt = int(arrayStr)
+    return arrayInt + 1
